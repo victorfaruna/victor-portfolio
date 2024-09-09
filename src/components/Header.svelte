a6fe42a8030c8ac5a52ee9f35b41ed990e2aed8a
victorfaruna <farunavictor043@gmail.com>
2024-09-09 10:43:37 +0100

    yes boss

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -16,7 +16,7 @@
 
 <header class="w-full h-[100px] flex items-center px-[5%]">
 	<div
-		class="inner w-full p-3 bg-color-1/[0.0001] backdrop-blur-[2px] rounded-2xl border border-color-1/10 flex justify-between shadow-md shadow-color-1/[0.02]"
+		class="inner w-full p-3 bg-color-1/[0.0001] backdrop-blur-[2px] rounded-2xl border border-color-1/10 flex justify-between items-center shadow-md shadow-color-1/[0.02]"
 	>
 		<Logo />
 		<nav class="sm:hidden flex items-center">
@@ -68,7 +68,7 @@
 			<ThemeToggle />
 			<button
 				name="Contact me"
-				class="btn w-[120px] h-[30px] rounded-lg bg-color-2 text-[11px] flex items-center justify-center gap-1 font-medium text-white"
+				class="w-[120px] h-[30px] rounded-lg bg-color-2 text-[11px] flex items-center justify-center gap-1 font-medium text-white"
 				on:click={() => goto('https://wa.me/2349125165937')}
 			>
 				<span>Contact Me</span>
